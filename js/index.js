16ddf75b0689586eff006416b467a0aef099b9ef
dujunhui007 <810743626@qq.com>
2018-01-11 10:28:31 +0800

    small change

--- js/index.js
+++ js/index.js
@@ -31,13 +31,13 @@ $(document).ready(function () {
     $("#downloads .downloadsContainer .downloadsContainerRight").css("height", "calc(100% + 50px)");
     $("#downloads .downloadsContainer .downloadsContainerLeft .dlsit-list li .dlistContainer").css("margin", "40px 2px");
 
-    if ((screen.width >1800) && (screen.width< 1910)){
-      $("#home .aschHomeImg1").css({"width": "736px", "top": "32%", "left": "calc(50% - 368px)"});
-      $("#home .aschHomeLinks").css({"width": "450px", "top": "60%", "left": "calc(50% - 225px)"});
-    }else {
-      $("#home .aschHomeImg1").css({"width": "736px", "top": "32%", "left": "calc(50% - 368px)"});
-      $("#home .aschHomeLinks").css({"width": "450px", "top": "70%", "left": "calc(50% - 225px)"});
-    }
+    // if ((screen.width >1800) && (screen.width< 1910)){
+    //   $("#home .aschHomeImg1").css({"width": "736px", "top": "32%", "left": "calc(50% - 368px)"});
+    //   $("#home .aschHomeLinks").css({"width": "450px", "top": "60%", "left": "calc(50% - 225px)"});
+    // }else {
+    //   $("#home .aschHomeImg1").css({"width": "736px", "top": "32%", "left": "calc(50% - 368px)"});
+    //   $("#home .aschHomeLinks").css({"width": "450px", "top": "70%", "left": "calc(50% - 225px)"});
+    // }
   }
 
   if (isEdge > -1) {
